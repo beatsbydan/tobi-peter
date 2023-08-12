bc932d4a22d4d402640c3d5bcc7b1e6d03b1c571
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2023-08-12 18:10:52 +0100

    Update

--- Frontend/src/Components/User/Pages/Shows/Show/Show.js
+++ Frontend/src/Components/User/Pages/Shows/Show/Show.js
@@ -3,7 +3,9 @@ import {BsTicketPerforated} from 'react-icons/bs'
 import {BiRightArrowAlt} from 'react-icons/bi'
 
 const Show = (props) => {
-    const timer = 1200 * props.myId
+    const duration = 1000; 
+    const delay = 500;
+    const animateShow = (i) => `loaded ${duration}ms cubic-bezier(0,-0.34, 0, 1.33) ${delay * (i+1)}ms forwards`;
     const date = new Date(props.date)
     const myDate = date.getDate().toString()
     const myMonth = date.getMonth()
@@ -13,26 +15,24 @@ const Show = (props) => {
     }
     const txtMonth = getMonth(myMonth)
     return ( 
-        setTimeout(()=>{
-            <li className='loaded show'>
-                <div className='left'>
-                    <div className='date'>
-                        <small className='month'>{txtMonth}</small>
-                        <h5 className='day'>{myDate}</h5>
-                    </div>
-                    <h5 className='desc'>DJ</h5>
-                    <div className='location'>
-                        <h5 className='title'>{props.title}</h5>
-                        <small className='venue'>{props.venue}</small>
-                    </div>    
+        <li className='show' style={{ animation: animateShow(props.myId) }}>
+            <div className='left'>
+                <div className='date'>
+                    <small className='month'>{txtMonth}</small>
+                    <h5 className='day'>{myDate}</h5>
                 </div>
-                {props.ticketLink && <a target='_blank' rel="noreferrer" href={props.ticketLink}>
-                    <BsTicketPerforated className={'ticket'} size={30}/>
-                    <BiRightArrowAlt className={'myArrow'}/>
-                    <span>GET TICKETS</span>
-                </a>}
-            </li>
-        },1200)
+                <h5 className='desc'>DJ</h5>
+                <div className='location'>
+                    <h5 className='title'>{props.title}</h5>
+                    <small className='venue'>{props.venue}</small>
+                </div>    
+            </div>
+            {props.ticketLink && <a target='_blank' rel="noreferrer" href={props.ticketLink}>
+                <BsTicketPerforated className={'ticket'} size={30}/>
+                <BiRightArrowAlt className={'myArrow'}/>
+                <span>GET TICKETS</span>
+            </a>}
+        </li>
     );
 }
 export default Show;
